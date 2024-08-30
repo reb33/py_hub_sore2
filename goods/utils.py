9603324b2e94718d5adaba8d329e17f54a8c1ab2
k.barylnikov <ipt_git_bot@ipt.ru>
2024-08-30 19:05:54 +0300

    fulltext search with poatgres db

--- goods/utils.py
+++ goods/utils.py
@@ -1,3 +1,4 @@
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from django.db.models import Q
 
 from goods.models import Products
@@ -8,11 +9,16 @@ def q_search(query: str):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
 
-    keywords = [word for word in query.split() if len(word) > 2]
+    # return Products.objects.annotate(search=SearchVector("name", "description")).filter(search=query) # без сортировки
+    vector = SearchVector("name", "description")
+    query = SearchQuery(query)
+    return Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")  # сортировка по релевантности
 
-    q_objects = Q()
-    for token in keywords:
-        q_objects |= Q(description__icontains=token)
-        q_objects |= Q(name__icontains=token)
-
-    return Products.objects.filter(q_objects)
+    # keywords = [word for word in query.split() if len(word) > 2]
+    #
+    # q_objects = Q()
+    # for token in keywords:
+    #     q_objects |= Q(description__icontains=token)
+    #     q_objects |= Q(name__icontains=token)
+    #
+    # return Products.objects.filter(q_objects)
--- store/settings.py
+++ store/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
 
     'main',
     "goods",
@@ -86,10 +87,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'store',
-        'USER': 'postgres',
+        'USER': 'user',
         'PASSWORD': 'password',
         'HOST': 'localhost',
-        'PORT': '5432'
+        'PORT': '5433'
     }
 }
 
