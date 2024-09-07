8af6d3c231321e98bd12481f25f05457b7a112db
k.barylnikov <ipt_git_bot@ipt.ru>
2024-09-07 20:32:21 +0300

    profile form and controller, add save image

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 
 from users.models import User
 
@@ -54,3 +54,21 @@ class UserRegistrationForm(UserCreationForm):
     password1 = forms.CharField()
     password2 = forms.CharField()
 
+
+class ProfileForm(UserChangeForm):
+
+    class Meta:
+        model = User
+        fields = [
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "image",
+        ]
+
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
+    image = forms.ImageField(required=False)
--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserRegistrationForm, ProfileForm
 
 
 def login(request):
@@ -43,8 +43,21 @@ def registration(request):
 
 
 def profile(request):
+    if request.method == "POST":
+        form = ProfileForm(
+            data=request.POST,
+            instance=request.user,  # передаем авторизованного пользователя
+            files=request.FILES,  # для сохранения файлов
+        )
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('user:profile'))
+    else:
+        form = ProfileForm(instance=request.user)  # передаем авторизованного пользователя
+
     context = {
         'title': 'Home - Кабинет',
+        "form": form,
     }
     return render(request, 'users/profile.html', context)
 
