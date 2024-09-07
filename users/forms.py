0ee8a6750d2b929a9961ab9e55d377375f4dc4ce
k.barylnikov <ipt_git_bot@ipt.ru>
2024-09-07 19:08:45 +0300

    login form and controller

--- users/forms.py
+++ users/forms.py
@@ -5,5 +5,31 @@ from users.models import User
 
 
 class UserLoginForm(AuthenticationForm):
+    username = forms.CharField()
+    password = forms.CharField()
+
     class Meta:
         model = User
+        fields = ["username", "password"]
+
+    # username = forms.CharField(  # для отрисовки через {{ form }}
+    #     label="Имя пользователя",
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             "autofocus": True,
+    #             "class": "form-control",
+    #             "placeholder": "Введите ваше имя пользователя",
+    #         }
+    #     )
+    # )
+    # password = forms.CharField(
+    #     label="Пароль",
+    #     widget=forms.PasswordInput(
+    #         attrs={
+    #             "autocomplete": "current-password",
+    #             "class": "form-control",
+    #             "placeholder": "Введите пароль",
+    #         }
+    #     ),
+    # )
+
