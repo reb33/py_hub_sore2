fae91390e55bd52c7a0486b305186a90d72212e4
k.barylnikov <ipt_git_bot@ipt.ru>
2024-09-18 21:55:48 +0300

    change view add_cart with ajax

--- carts/templatetags/carts_tags.py
+++ carts/templatetags/carts_tags.py
@@ -1,11 +1,10 @@
 from django import template
 
-from carts.models import Cart
+from carts.utils import get_user_carts
 
 register = template.Library()
 
 
 @register.simple_tag()
-def get_cats_for_user(request):
-    if request.user.is_authenticated:
-        return Cart.objects.filter(user=request.user)
+def get_carts_for_user(request):
+    return get_user_carts(request)
